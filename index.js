0258ec6b7f17b0b1092031181f48c18e2538cedd
Samiksha Sanjay Baral <baral.sam@northeastern.edu>
2024-02-13 16:12:22 -0500

    Modified code to validate databases

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import healthCheckRoutes from './routes/healthCheckRoutes.js';
 import userRoutes from './routes/userRoutes.js'
-import {validateAndCreateDatabase} from './services/healthCheckServices.js'
 import { createExpressApp } from './utilities/healthCheckUtils.js';
 
-await validateAndCreateDatabase();
-const app = createExpressApp();
+const app = await createExpressApp();
 
 app.use('/healthz', healthCheckRoutes);
 app.use('/v1/user', userRoutes);
--- utilities/healthCheckUtils.js
+++ utilities/healthCheckUtils.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { sequelize } from '../services/healthCheckServices.js';
+import {validateAndCreateDatabase} from '../services/healthCheckServices.js'
 
-export const createExpressApp = () => {
+export const createExpressApp = async() => {
   const app = express();
   app.use(express.json());
+  await validateAndCreateDatabase();
   sequelize.sync({alter : true});
   return app;
 };
