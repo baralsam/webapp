956eea370ac2379afca5c7ad66dcaf62b422e9fc
Samiksha Sanjay Baral <baral.sam@northeastern.edu>
2024-03-19 18:43:24 -0400

    removed permission

--- setup.sh
+++ setup.sh
@@ -41,6 +41,4 @@ sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 # Copy the config yaml and restart google cloud ops agent
 sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml 
-sudo chown csye6225:csye6225 /etc/google-cloud-ops-agent/config.yaml
-sudo chmod 644 /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
