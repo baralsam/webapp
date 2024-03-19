152db5ecb909750bb9c5235314c2ef83202aada1
Samiksha Sanjay Baral <baral.sam@northeastern.edu>
2024-03-19 17:48:29 -0400

    modified shell

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ sudo curl -O https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.s
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 # Copy the config yaml and restart google cloud ops agent
-sudo cp /etc/google-cloud-ops-agent/config.yaml/tmp/config.yaml 
+sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml 
 sudo chown csye6225:csye6225 /etc/google-cloud-ops-agent/config.yaml
 sudo chmod 644 /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
