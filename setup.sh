735a706eb4947d54d85994616f0784cc37c98a43
Samiksha Sanjay Baral <baral.sam@northeastern.edu>
2024-03-19 23:44:35 -0400

    removed error

--- setup.sh
+++ setup.sh
@@ -41,4 +41,3 @@ sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
 # Copy the config yaml and restart google cloud ops agent
 sudo cp /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml 
-sudo systemctl restart google-cloud-ops-agent
